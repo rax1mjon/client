bd523175b8b676eb32f2686c6cb289221056fef8
rax1mjon <tulaganovraximjon65@gmail.com>
2024-11-05 22:34:50 +0500

    big project

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// const loading = document.getElementById("loading");
+
+// const loadingDuration = 10000;
+
+// setTimeout(() => {
+//     loading.classList.add("loading-none");
+// }, loadingDuration);
+
+
+// window.addEventListener("scroll", function () {
+//     shrink();
+// });
+
+// let navbar = document.getElementById("header");
+
+// function shrink() {
+//     if (scrollY > 780) {
+//         navbar.classList.add("navbar-shrink");
+//     } else {
+//         navbar.classList.remove("navbar-shrink");
+//     }
+// }
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const menuToggle = document.getElementById('humburger');
+//     const navLinks = document.getElementById('nav--bottom');
+
+//     menuToggle.addEventListener('click', () => {
+//         navLinks.classList.toggle('active');
+//     });
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('humburger');
+    const navLinks = document.getElementById('nav--bottom');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('activeh');
+    });
+});
+
+
+// indow.addEventListener("scroll", function () {
+//     toggleBacktop();
+// });
+
+// let backtop = document.getElementById("backtop");
+
+// function toggleBacktop() {
+//     if (
+//         document.body.scrollTop > 150 ||
+//         document.documentElement.scrollTop > 150
+//     ) {
+//         backtop.classList.add('backtop-show')
+//     } else {
+//         backtop.classList.remove('backtop-show')
+//     }
+// }
